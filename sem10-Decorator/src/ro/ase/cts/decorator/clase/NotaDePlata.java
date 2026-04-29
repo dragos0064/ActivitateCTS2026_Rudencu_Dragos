c1c4e7599c37197edb6226890d6e64e96066e117
Dragos Rudencu <dragosrudencu24@gmail.com>
2026-04-29 10:29:36 +0300

    sem10-final

---
+++ sem10-Decorator/src/ro/ase/cts/decorator/clase/NotaDePlata.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.decorator.clase;
+
+public class NotaDePlata implements NotaDePlataAbstract{
+    private Float suma;
+    private String data;
+
+    public NotaDePlata(Float suma, String data) {
+        this.suma = suma;
+        this.data = data;
+    }
+
+    @Override
+    public void printeaza() {
+        System.out.println("\nLa data "+ data + " a fost platita suma de "+suma);
+    }
+}
---
+++ sem10-Decorator/src/ro/ase/cts/decorator/decorator/NotaDePlataDecorator.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.decorator.decorator;
+
+import ro.ase.cts.decorator.clase.NotaDePlataAbstract;
+
+public abstract class  NotaDePlataDecorator implements NotaDePlataAbstract {
+    private NotaDePlataAbstract notaDePlata;
+
+    public NotaDePlataDecorator(NotaDePlataAbstract notaDePlata) {
+        this.notaDePlata = notaDePlata;
+    }
+
+
+    @Override
+    public void printeaza() {
+        notaDePlata.printeaza();
+    }
+
+    public void printeazaFelicitare(){}
+}
