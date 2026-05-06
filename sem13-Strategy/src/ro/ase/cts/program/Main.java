983f1614554dba97e78bb43d74cc4603c0681fc0
Dragos Rudencu <dragosrudencu24@gmail.com>
2026-05-06 10:44:43 +0300

    sem13-final

---
+++ sem13-Strategy/src/ro/ase/cts/program/Main.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.program;
+
+
+import ro.ase.cts.clase.ProbaOrala;
+import ro.ase.cts.clase.ProbaScrisa;
+import ro.ase.cts.clase.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Marcel");
+        student.examinare();
+
+        student.setModSustinere(new ProbaScrisa());
+        student.examinare();
+
+        student.setModSustinere(new ProbaOrala());
+        student.examinare();
+    }
+}
